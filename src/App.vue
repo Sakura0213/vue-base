493c147e20c89a413ade225ac137d9531760a77a
guapizx <zengxin@email.cn>
2023-08-06 19:19:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<hello />
+</template>
+
+<script >
+import hello from './components/hello.vue'
+export default{
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    hello
+  }
+}
+</script>
+
